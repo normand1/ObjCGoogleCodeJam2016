b58efd4d4cd4116eae6e3e89441135a9460e90b8
davidnorman <david.norman.w@gmail.com>
2016-04-08 20:54:39 -0700

    Initial Commit

---
+++ codeJam2016/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  codeJam2016
+//
+//  Created by David Norman on 4/8/16.
+//  Copyright © 2016 David Norman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
